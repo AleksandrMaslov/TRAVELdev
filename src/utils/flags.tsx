6e804b107922fbc154165e2421283eb37ddac900
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-14 20:54:20 +0300

    add: country flags

---
+++ src/utils/flags.tsx
@@ -0,0 +1,27 @@
+import {
+  SvgFlagBali,
+  SvgFlagBelarus,
+  SvgFlagChina,
+  SvgFlagEgypt,
+  SvgFlagItaly,
+  SvgFlagPhilipines,
+  SvgFlagRussia,
+  SvgFlagSingapore,
+  SvgFlagTurkey,
+  SvgFlagVietnam,
+} from 'src/assets'
+
+const flags = {
+  bali: <SvgFlagBali />,
+  belarus: <SvgFlagBelarus />,
+  china: <SvgFlagChina />,
+  egypt: <SvgFlagEgypt />,
+  italy: <SvgFlagItaly />,
+  philipines: <SvgFlagPhilipines />,
+  russia: <SvgFlagRussia />,
+  singapore: <SvgFlagSingapore />,
+  turkey: <SvgFlagTurkey />,
+  vietnam: <SvgFlagVietnam />,
+}
+
+export default flags
