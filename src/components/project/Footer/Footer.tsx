5f9de99416fca183c7c32cc20772ef8852de2e83
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-18 00:46:20 +0300

    add: Footer animation

--- src/components/project/Footer/Footer.tsx
+++ src/components/project/Footer/Footer.tsx
@@ -1,8 +1,30 @@
+import { motion } from 'framer-motion'
 import { FC } from 'react'
 
 import { SvgFooterHighlighter, SvgMountains, SvgUnderline } from 'src/assets'
 
 import classes from './Footer.module.css'
+
+const variantsCenter = {
+  hidden: { opacity: 0, x: '-50%', y: '-20%' },
+  visible: (i: number) => {
+    return { opacity: 1, x: '-50%', y: '-50%', transition: { delay: i * 0.2 } }
+  },
+}
+
+const variantsBottom = {
+  hidden: { opacity: 0, y: 100 },
+  visible: (i: number) => {
+    return { opacity: 1, y: 0, transition: { delay: i * 0.2 } }
+  },
+}
+
+const variantsImg = {
+  hidden: { opacity: 0, x: 200 },
+  visible: (i: number) => {
+    return { opacity: 1, x: 0, transition: { delay: i * 0.2 } }
+  },
+}
 
 const Footer: FC = () => {
   const { footer, container, wrapper, text, backgroundText } = classes
@@ -11,21 +33,39 @@ const Footer: FC = () => {
     <footer className={footer}>
       <div className={container}>
         <div className={wrapper}>
-          <h1 className={text}>
+          <motion.h1
+            className={text}
+            initial="hidden"
+            whileInView="visible"
+            custom={1}
+            variants={variantsCenter}
+          >
             <pre>Все о чем мы</pre>
             <pre>можем мечтать</pre>
             <pre>- реально</pre>
-          </h1>
+          </motion.h1>
 
           <SvgFooterHighlighter />
         </div>
 
-        <SvgMountains />
+        <SvgMountains
+          initial="hidden"
+          whileInView="visible"
+          custom={2}
+          variants={variantsImg}
+        />
       </div>
 
-      <h1 className={backgroundText}>
+      <motion.h1
+        className={backgroundText}
+        initial="hidden"
+        whileInView="visible"
+        custom={0.5}
+        variants={variantsBottom}
+      >
         мурр мурр мурр мурр мурр мурр мурр мурр
-      </h1>
+      </motion.h1>
+
       <SvgUnderline />
     </footer>
   )
