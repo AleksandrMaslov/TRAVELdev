bdfa05a11f30990afbf49c44f9649dc6420ab16f
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-18 01:51:15 +0300

    add: TripAlbums loader animation

--- src/components/project/TripsAlbums/TripsAlbums.tsx
+++ src/components/project/TripsAlbums/TripsAlbums.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import { FC, Suspense } from 'react'
 import { Await, LoaderFunction, defer, useLoaderData } from 'react-router-dom'
 
@@ -8,28 +9,45 @@ import { List, TripItem, YearItem } from '..'
 
 import classes from './TripsAlbums.module.css'
 
+const variants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: (i: number) => {
+    return { opacity: 1, x: 0, transition: { delay: i * 0.1 } }
+  },
+}
+
 const TripsAlbums: FC = () => {
   const { data } = useLoaderData() as { data: [string, ITrip[]][] }
 
   return (
     <Suspense
       fallback={
-        <h2 className={classes.loader}>Вспоминаем наши путешествия...</h2>
+        <motion.h2
+          className={classes.loader}
+          initial="hidden"
+          whileInView="visible"
+          variants={variants}
+          custom={1}
+        >
+          Вспоминаем наши путешествия...
+        </motion.h2>
       }
     >
       <Await resolve={data}>
         {resolvedData => (
-          <List
-            items={resolvedData}
-            item={([year, trips]: [string, ITrip[]]) => (
-              <YearItem
-                key={year}
-                title={year}
-                items={trips}
-                item={(trip: ITrip) => <TripItem key={trip.id} trip={trip} />}
-              />
-            )}
-          />
+          <AnimatePresence mode="wait">
+            <List
+              items={resolvedData}
+              item={([year, trips]: [string, ITrip[]]) => (
+                <YearItem
+                  key={year}
+                  title={year}
+                  items={trips}
+                  item={(trip: ITrip) => <TripItem key={trip.id} trip={trip} />}
+                />
+              )}
+            />
+          </AnimatePresence>
         )}
       </Await>
     </Suspense>
