d3496ec87cd763e5ea71ff74ba157b35f69614aa
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-18 00:24:21 +0300

    add: Header animation

--- src/components/project/Header/Header.tsx
+++ src/components/project/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import { FC } from 'react'
 
 import { Logo, TempBurger } from 'src/components/project'
@@ -9,17 +10,28 @@ interface HeaderProps {
   className?: string
 }
 
+const variants = {
+  hidden: (x: number) => {
+    return { opacity: 0, x }
+  },
+  visible: { opacity: 1, x: 0 },
+}
+
 const Header: FC<HeaderProps> = ({ className }) => {
   const { header, container } = classes
   const headerClasses = useStyles(header, className)
 
   return (
-    <header className={headerClasses}>
+    <motion.header
+      initial="hidden"
+      whileInView="visible"
+      className={headerClasses}
+    >
       <div className={container}>
-        <Logo />
-        <TempBurger />
+        <Logo custom={-100} variants={variants} />
+        <TempBurger custom={100} variants={variants} />
       </div>
-    </header>
+    </motion.header>
   )
 }
 
