d209baecb64fd28460459e7a11990f1ccd6b53d2
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-17 20:12:57 +0300

    refcator: Tab styling with hover

--- src/components/project/Tab/Tab.tsx
+++ src/components/project/Tab/Tab.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react'
 import { Link, useMatch } from 'react-router-dom'
 
 import { SvgTabsHighlighter } from 'src/assets'
+import { useStyles } from 'src/hooks'
 
 import classes from './Tab.module.css'
 
@@ -10,18 +11,16 @@ interface TabProps {
   to: string
 }
 
-const linkStyle = (isActive: boolean) => {
-  return isActive ? { fontWeight: 600, letterSpacing: '-0.007rem' } : undefined
-}
-
 const Tab: FC<TabProps> = props => {
-  const { tab, highlight } = classes
+  const { tab, text, text_active, highlight } = classes
   const { children, to } = props
+
   const isActive = useMatch(to)
+  const textStyles = useStyles(text, isActive ? text_active : undefined)
 
   return (
     <Link to={to} className={tab}>
-      <h5 style={linkStyle(!!isActive)}>{children}</h5>
+      <h5 className={textStyles}>{children}</h5>
       {isActive && <SvgTabsHighlighter className={highlight} />}
     </Link>
   )
