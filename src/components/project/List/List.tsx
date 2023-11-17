e6863bb39ce25064e7fad2148d1ca7f85c820882
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-18 02:40:29 +0300

    refactor: List animation with MotionProps

--- src/components/project/List/List.tsx
+++ src/components/project/List/List.tsx
@@ -1,15 +1,20 @@
+import { MotionProps, motion } from 'framer-motion'
 import { ReactNode } from 'react'
 
 import classes from './List.module.css'
 
-interface ListProps<T> {
+interface ListProps<T> extends MotionProps {
   items?: [string, T[]][]
   item: (item: [string, T[]]) => ReactNode
 }
 
 function List<T>(props: ListProps<T>) {
-  const { items = [], item } = props
-  return <div className={classes.list}>{items.map(item)}</div>
+  const { items = [], item, ...motionProps } = props
+  return (
+    <motion.div className={classes.list} {...motionProps}>
+      {items.map(item)}
+    </motion.div>
+  )
 }
 
 export default List
