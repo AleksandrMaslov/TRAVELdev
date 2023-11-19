38ce7b96e4e7e96b30962717333493b55d97eaa7
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-19 17:39:39 +0300

    add: Motto animation

--- src/components/project/Motto/Motto.tsx
+++ src/components/project/Motto/Motto.tsx
@@ -1,21 +1,33 @@
+import { motion } from 'framer-motion'
 import { FC } from 'react'
 
 import { SvgMottoHighlighter } from 'src/assets'
 
 import classes from './Motto.module.css'
 
+const variants = {
+  hidden: { opacity: 0, y: 100 },
+  visible: { opacity: 1, y: 0, transition: { delay: 0.1, duration: 0.3 } },
+}
+
 const Motto: FC = () => {
   const { motto, highlight } = classes
 
   return (
-    <section className={motto}>
+    <motion.section
+      className={motto}
+      initial="hidden"
+      whileInView="visible"
+      variants={variants}
+      viewport={{ once: true }}
+    >
       <h1>Даем клятву кайфовать всю жизнь!</h1>
       <h1>Любить, удивлять, ворчать и обожать =)</h1>
       <h1>Держать друг друга за руку до старости!</h1>
       <h1>Целовать, поддерживать и обнимать!</h1>
 
       <SvgMottoHighlighter className={highlight} />
-    </section>
+    </motion.section>
   )
 }
 
