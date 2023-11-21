8e41d5855d5bedf11bda733fb4b5752fe94c3910
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-21 10:54:48 +0300

    fix: getErrorMessage

--- src/utils/getErrorMessage.ts
+++ src/utils/getErrorMessage.ts
@@ -1,5 +1,8 @@
 export default function getErrorMessage(error: unknown): string {
   if (error instanceof Error) return error.message
-  if (error instanceof Object) return `${error.status} ${error.statusText}`
+  if (error instanceof Object) {
+    if ('status' in error && 'statusText' in error)
+      return `${error.status} ${error.statusText}`
+  }
   return String(error)
 }
