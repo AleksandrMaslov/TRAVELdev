404bce668b7eb4c2e8290c1be176e615952fed97
AleksandrMaslov <amaslov8910@gmail.com>
2023-11-21 12:25:39 +0300

    add: Cover bg and border radius

--- src/components/project/Cover/Cover.tsx
+++ src/components/project/Cover/Cover.tsx
@@ -1,11 +1,18 @@
 import { FC } from 'react'
 
+import bg from 'src/assets/bg.jpg'
+import { Image } from 'src/components/UI'
+
 import classes from './Cover.module.css'
 
 const Cover: FC = () => {
-  const { cover } = classes
+  const { cover, img } = classes
 
-  return <section className={cover}></section>
+  return (
+    <section className={cover}>
+      <Image src={bg} className={img} />
+    </section>
+  )
 }
 
 export default Cover
